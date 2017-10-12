0477c2e4c060a4c53ba222d9108dcc071e294a91
Orry Verducci <orry@orryverducci.co.uk>
2017-10-12 19:41:44 +0100

    Change to platform prefered directory separator (#6)
    Fixes issue with CA bundle not being found on macOS systems

--- src/UnionCloud/Api.php
+++ src/UnionCloud/Api.php
@@ -31,7 +31,7 @@ class Api {
         
         // verify ssl certs incase server misconfigured
         $guzzleClient = new GuzzleClient(array(
-            "verify" => dirname(__FILE__, 3) . "\unioncloud.pem"
+            "verify" => dirname(__FILE__, 3) . DIRECTORY_SEPARATOR . "unioncloud.pem"
         ));
         $this->client->setClient($guzzleClient);
         
